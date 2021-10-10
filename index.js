58c11a95bc1ea4685a2a971a76046f92b47be337
Johan Samper Gómez <johansamperg@gmail.com>
2021-10-10 15:01:33 -0500

    ajuste cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import Usuario from './data/data.js'
 import express from 'express';
 var port = process.env.PORT || 3000;
-var cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+
 app.get('/api/registrar', function(req, res) {
     try {
         const email = req.query.email;
